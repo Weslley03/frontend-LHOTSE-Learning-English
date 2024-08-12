99e866e2ed6656995a5a34dc5bfa9f0ff31f8e07
oweslley03 <wesleymoraeswv03@hotmail.com>
2024-08-12 14:44:11 -0300

    leitor de audio OK

--- src/components/CardContentListen/CardContentTextListen.tsx
+++ src/components/CardContentListen/CardContentTextListen.tsx
@@ -6,6 +6,7 @@ import {
   TextAreaContent,
 } from "../CardContent/CardContentStyled";
 import React, { useRef, useEffect } from "react";
+import { ButtonTrain } from "../buttonTrain/ButtonTrainStyled";
 
 type CardContentProps = {
   tituloText: string;
@@ -17,8 +18,11 @@ const CardContentTextListen: React.FC<CardContentProps> = ({
   textAreaValue,
 }) => {
   const textareaRef = useRef<HTMLTextAreaElement | null>(null);
+  const textListen: any = localStorage.getItem("textListen");
+  var audio: any;
 
   useEffect(() => {
+    textToSpeach();
     const textarea: any = textareaRef.current;
     if (!textarea) return;
     const handleInput = () => {
@@ -33,7 +37,21 @@ const CardContentTextListen: React.FC<CardContentProps> = ({
     };
   }, []);
 
-  const textListen: any = localStorage.getItem("textListen");
+  function pauseAudio() {
+    const storedAudioUrl = localStorage.getItem("audioUrl");
+    if (storedAudioUrl) {
+      const audio = new Audio(storedAudioUrl);
+      audio.pause(); // Play the audio
+    }
+  }
+
+  function playAudio() {
+    const storedAudioUrl = localStorage.getItem("audioUrl");
+    if (storedAudioUrl) {
+      const audio = new Audio(storedAudioUrl);
+      audio.play(); // Play the audio
+    }
+  }
 
   async function textToSpeach() {
     try {
@@ -45,8 +63,9 @@ const CardContentTextListen: React.FC<CardContentProps> = ({
       );
       const audioBlob = new Blob([response.data], { type: "audio/mpeg" });
       const audioUrl = URL.createObjectURL(audioBlob);
+      localStorage.setItem("audioUrl", audioUrl);
 
-      const audio = new Audio(audioUrl);
+      audio = new Audio(audioUrl);
       audio.play();
     } catch (err) {
       console.log("deu erro", err);
@@ -64,6 +83,8 @@ const CardContentTextListen: React.FC<CardContentProps> = ({
         ref={textareaRef}
         defaultValue={textAreaValue}
       />
+      <ButtonTrain onClick={pauseAudio}>pause</ButtonTrain>
+      <ButtonTrain onClick={playAudio}>play</ButtonTrain>
     </Container>
   );
 };
